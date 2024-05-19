c967dafa6147a65212b415f50c2627c8cf65faaa
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-05-19 12:26:44 +0530

    dom manupilation properties

--- DOM.js
+++ DOM.js
@@ -1,6 +1,4 @@
-console.log(window);
-window.console.log("hello");
-window.alert("hello");
+
 //window object -window object represents an open window in a browser.it is brower's object (not javascript object)
 //and is automatic created by browser.
 //it is a global object with lots of properties and methods.
@@ -9,5 +7,25 @@ window.alert("hello");
 //console.dir-> document ->used to print properties and methods of documents.
 //when a web page is loaded,the browser creates a documents object model(dom) of the page
 
-let heading=document.getElementById("heading");
-console.dir(heading);
+let heading1=document.getElementById("heading1");
+console.dir(heading1);
+let heading2=document.getElementsByClassName("heading2");
+console.dir(heading2);
+console.log(heading2);
+let parahs=document.getElementsByTagName("p");
+console.dir(parahs);
+console.log(parahs);
+let elements=document.querySelector("p");
+console.dir(elements);
+let allelements=document.querySelectorAll("p");
+console.dir(elements);
+
+let ele=document.querySelector(".heading2");
+console.dir(ele);
+let el=document.querySelector(".heading1");
+console.dir(el);
+
+
+
+
+
