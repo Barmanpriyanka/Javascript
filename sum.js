bff1aee9e4e3f9b32cf1645fae194bf91b2daa1c
Barmanpriyanka <priyankabarman1604@gmail.com>
2025-01-11 12:42:46 +0530

    swap the value of two varaible without using third varaible .

--- sum.js
+++ sum.js
@@ -7,14 +7,13 @@ console.log(sum(5,7));
 
 function swap(a,b)
 {
-    let temp;
-    console.log("before swaping  value of a",a);
-    console.log("before swaping  value of b",b);
-    temp=a;
-    a=b;
-    b=temp;
-    console.log("after swaping value of a",a);
-    console.log("after swaping value of b",b);
+    
+    console.log("before swaping  value of a and b are: ",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    console.log("after swaping value of a and b are :",a,b);
+    
 
 }
 swap(4,5);
