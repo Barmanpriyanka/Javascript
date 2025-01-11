557d4be2bc74eadee1a0c2d210b57881c645fada
Barmanpriyanka <priyankabarman1604@gmail.com>
2025-01-11 12:28:14 +0530

    Swaping of two variable in js using third variable.

--- sum.js
+++ sum.js
@@ -4,3 +4,18 @@ function sum(x,y)
     return x+y;
 }
 console.log(sum(5,7));
+
+function swap(a,b)
+{
+    let temp;
+    console.log("before swaping  value of a",a);
+    console.log("before swaping  value of b",b);
+    temp=a;
+    a=b;
+    b=temp;
+    console.log("after swaping value of a",a);
+    console.log("after swaping value of b",b);
+
+}
+swap(4,5);
+
